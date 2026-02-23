98d77285233e2a8dd783596bbdeed45a144f4187
Kelvin Chinagorom <116084116+klintech@users.noreply.github.com>
2026-02-23 06:47:36 -0800

    Delete core directory

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from .views import main_app
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', main_app, name='app')
-]
